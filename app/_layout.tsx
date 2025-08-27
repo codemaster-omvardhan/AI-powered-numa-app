f3bc9eb95c238168d29a2b48059385588dacc71e
Om Vardhan <omvardhan5@gmail.com>
2025-08-27 20:35:37 +0530

    Create a new AI agent

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { ElevenLabsProvider } from "@elevenlabs/react-native";
 import { Stack } from "expo-router";
 
 function RootLayoutWithAuth() {
@@ -11,15 +12,17 @@ function RootLayoutWithAuth() {
   }
 
   return (
-    <Stack>
-      <Stack.Protected guard={isSignedIn}>
-        <Stack.Screen name="(protected)" options={{ headerShown: false }} />
-      </Stack.Protected>
+    <ElevenLabsProvider>
+      <Stack>
+        <Stack.Protected guard={isSignedIn}>
+          <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+        </Stack.Protected>
 
-      <Stack.Protected guard={!isSignedIn}>
-        <Stack.Screen name="(public)" options={{ headerShown: false }} />
-      </Stack.Protected>
-    </Stack>
+        <Stack.Protected guard={!isSignedIn}>
+          <Stack.Screen name="(public)" options={{ headerShown: false }} />
+        </Stack.Protected>
+      </Stack>
+    </ElevenLabsProvider>
   );
 }
 

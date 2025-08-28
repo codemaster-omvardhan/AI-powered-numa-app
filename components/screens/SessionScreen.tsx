c2d17ded3e4260fc1fc28c7d3a71805ad4f1e2c0
Om Vardhan <omvardhan5@gmail.com>
2025-08-28 16:53:18 +0530

    Running on my real device

---
+++ components/screens/SessionScreen.tsx
@@ -0,0 +1,16 @@
+// import { View, Text } from 'react-native'
+// import React from 'react'
+// import { useConversation } from "@elevenlabs/react-native"
+
+// export default function SessionScreen() {
+//     const conversation = useConversation({
+//         onConnect: () => console.log("Connec");
+
+//     });
+
+//   return (
+//     <View>
+//       <Text>SessionScreen</Text>
+//     </View>
+//   )
+// }
